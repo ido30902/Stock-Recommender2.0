b4b9441a50015bdbbc7988ea284597884f0a5108
ido30902 <idobarabi@gmail.com>
2023-08-14 00:06:26 +0300

    first

---
+++ main.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import datetime as dt
+import matplotlib.pyplot as plt
+import yfinance as yf
+start = dt.datetime(1792,1,1)
+end = dt.datetime.now()
+
+def main():
+    tickers = load_tickers_list()
+    
+    
+
+
+def load_tickers_list():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
+
